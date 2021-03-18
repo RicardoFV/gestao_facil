20e3986d1fbf119657d1605f23bfa9bcd0152e70
Ricardo Ferreira <ricardo.ti.ads@gmail.com>
2021-03-18 17:17:17 -0300

    protegendo a aplicacao colocando que so pode acessar quem estiver autenticado

--- app/Http/Controllers/RequisitoController.php
+++ app/Http/Controllers/RequisitoController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class RequisitoController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
