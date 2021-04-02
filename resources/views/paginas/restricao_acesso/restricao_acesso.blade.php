aa03c659f16da4475e3bcdb4c7cbf732471a54e8
Ricardo Ferreira <ricardo.ti.ads@gmail.com>
2021-04-02 10:44:12 -0300

    corrigindo a mensangem de erro

--- resources/views/paginas/restricao_acesso/restricao_acesso.blade.php
+++ resources/views/paginas/restricao_acesso/restricao_acesso.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Acesso não Autorizado</h1>
-<a href="{{ url('/') }}" class="btn btn-block btn-success">Voltar ao Inicio</a>
+
+<div class="container">
+    <div class="card mt-1">
+        <div class="card-header">
+            <h1 class="text-center">Acesso Não Autorizado </h1>
+        </div>
+        <div class="form-row col-sm-12 justify-content-center">
+            <div class="form-group col-sm-6 d-flex inline mt-3">
+                <a href="{{ url('/') }}" class="btn btn-block btn-primary">Tela Incial</a>
+            </div>
+            
+        </div>
+
+        <!-- criação da tabela  -->
+        <div class="container">
+            <p class="text-center">
+                Você não tem acesso a essa atividade, verifique suas permissões com o 
+                Administrador do Sistema.
+            </p>
+        </div>
+    </div>
+</div>
 @endsection
