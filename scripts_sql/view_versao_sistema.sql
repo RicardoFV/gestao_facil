73f3f17ab0ddb39426168e5c348210f60d04bc11
Ricardo Ferreira <ricardo.ti.ads@gmail.com>
2021-04-21 22:46:07 -0300

    criando a view versao_sistema

---
+++ scripts_sql/view_versao_sistema.sql
@@ -0,0 +1,5 @@
+create or replace view v_versao_sistema as 
+select 
+v.id as id, s.id as id_sistema, v.nome as nome_versao , s.nome as nome_sistema, 
+s.descricao as descricao, s.excluido from versaos v inner join sistemas s 
+on v.id = s.id_versao ;
